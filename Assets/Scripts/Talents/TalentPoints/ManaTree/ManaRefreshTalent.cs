bbbc7d9a8ae73c8f759722d141831d5fea6cdfc0
Armandulis <32477822+Armandulis@users.noreply.github.com>
2023-08-08 22:52:41 +0200

    mana refresh cs file

---
+++ Assets/Scripts/Talents/TalentPoints/ManaTree/ManaRefreshTalent.cs
@@ -0,0 +1,44 @@
+public class ManaRefreshTalent: TalentPoint
+{
+    private HeroManager heroManager;
+
+    string id = "ManaRefreshTalent";
+    int currentLevel = 0;
+    int maxLevel = 5;
+
+
+    public ManaRefreshTalent(HeroManager heroManager)
+    {
+        this.heroManager = heroManager;
+        currentLevel = heroManager.GetHeroTalentsManager().manaRefreshLevel;
+    }
+
+    public int LevelUp()
+    {
+        if (currentLevel >= maxLevel) return maxLevel;
+        currentLevel++;
+        heroManager.GetHeroTalentsManager().manaRefreshLevel++;
+        heroManager.GetHeroManaManager().UpdateStatsForManaRefreshTalentLevel( currentLevel * 0.25f, 60 );
+        return currentLevel;
+    }
+
+    public string GetTitle()
+    {
+        return "Mana refresh";
+    }
+
+    public string GetDescription()
+    {
+        return "Refresh " + (((currentLevel + 1) * 0.25f ) * 100)+  "% of your mana" ;
+    }
+
+
+    public int GetMaximumTalentPoints()
+    {
+        return maxLevel;
+    }
+    public int GetCurrentTalentPoints()
+    {
+        return currentLevel;
+    }
+}
